1976927eb01b76e90048f466c0b879014545fee1
Underprotection <mertcandemirkaya@outlook.com>
2025-09-11 10:59:32 +0300

    04: AI Chat

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { UIMessage, streamText, convertToModelMessages } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = await streamText({
+      model: openai("gpt-5-nano"),
+      messages: convertToModelMessages(messages),
+    });
+
+    result.usage.then((usage) => {
+      console.log({
+        messageCount: messages.length,
+        inputTokens: usage.inputTokens,
+        outputTokens: usage.outputTokens,
+        totalTokens: usage.totalTokens,
+      });
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming chat completion:", error);
+    return Response.json(
+      { error: "Failed to stream chat completion" },
+      { status: 500 }
+    );
+  }
+}
